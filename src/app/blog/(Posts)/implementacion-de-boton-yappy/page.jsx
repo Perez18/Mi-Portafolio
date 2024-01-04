89354c2f595202e9465508b996d769a9bdf3679f
ANTHONY PEREZ <anthony.perez1@utp.ac.pa>
2024-01-04 03:53:21 -0500

    blog de implementacion de boton de yappy

--- src/app/blog/(Posts)/implementacion-de-boton-yappy/page.jsx
+++ src/app/blog/(Posts)/implementacion-de-boton-yappy/page.jsx
@@ -14,9 +14,50 @@ const page = () => {
         </p>
 
         <h2 className='text-3xl pt-5 font-bold'>Instalacion de SDK</h2>
-        <code>
-          npm install yappy-node-sdk-X.X.X.tgz
-        </code>
+        <pre className='bg-slate-400/10 py-6'>
+          <code>
+            npm install yappy-node-sdk-X.X.X.tgz
+          </code>
+        </pre>
+        <p>o</p>
+        <pre className='bg-slate-400/10 py-6 break-all'>
+          <code>
+            npm install https://github.com/BancoGeneral/Boton-de-Pago-Yappy_Node.js/releases/download/X.X.X/yappy-node-back-sdk-X.X.X.tar
+          </code>
+        </pre>
+
+        <h2 className='text-3xl pt-5 font-bold'>Importar SDK</h2>
+        <pre className='language-bash bg-slate-400/10 py-6' tabindex='0'>
+          <code className='language-bash'>
+            import * as yappy from "yappy-node-sdk";
+          </code>
+        </pre>
+
+        <h2 className='text-3xl pt-5 font-bold'>Obtener url de pago</h2>
+        <p>
+          En este punto, es crucial enviar nuestra información de pago al API para obtener un URL de pago. Este enlace nos dirigirá a la plataforma de Yappy.
+        </p>
+        <pre className='language-bash bg-slate-400/10 py-6' tabindex='0'>
+          <code className='language-bash'>
+            {` const payment= {
+              total: 20.17,
+              subtotal: 20.00,
+              shipping: 0.00,
+              discount: 0.00,
+              taxes: 0.17,
+              orderId: '1234',
+              successUrl: 'https://www.urlsuccess.com',
+              failUrl: 'https://www.urlfail.com',
+              tel: '61122345',
+              domain: 'https://www.tutienda.com',
+         }
+          const response = await yappyClient.getPaymentUrl(payment); // Pago normal
+          const response = await yappyClient.getPaymentUrl(payment,true); //Para donaciones
+          const response = await yappyClient.getPaymentUrl(payment,true, true); //Para donaciones, en modo prueba
+          const response = await yappyClient.getPaymentUrl(payment,false, true); //Pago normal, en modo prueba
+          `}
+          </code>
+        </pre>
       </div>
     </div>
   )
