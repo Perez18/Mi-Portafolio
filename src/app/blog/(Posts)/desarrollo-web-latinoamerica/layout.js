9b523befc463b2c087168d76f880e76209fb3c9f
ANTHONY PEREZ <anthony.perez1@utp.ac.pa>
2024-04-08 10:50:04 -0500

    feat: add openGrahp

--- src/app/blog/(Posts)/desarrollo-web-latinoamerica/layout.js
+++ src/app/blog/(Posts)/desarrollo-web-latinoamerica/layout.js
@@ -1,6 +1,10 @@
 export const metadata = {
   title: 'Desarrollo web en latinoamerica',
-  description: 'En los últimos años, el desarrollo web en Latinoamérica ha experimentado un crecimiento significativo, impulsado por el aumento en el acceso a Internet y la adopción de tecnologías digitales. Se observa una tendencia hacia la creación de sitios web responsivos, aplicaciones web progresivas (PWA) y la implementación de tecnologías como React, Angular, Node.js y Laravel en el desarrollo de aplicaciones web'
+  description: 'En los últimos años, el desarrollo web en Latinoamérica ha experimentado un crecimiento significativo, impulsado por el aumento en el acceso a Internet y la adopción de tecnologías digitales. Se observa una tendencia hacia la creación de sitios web responsivos, aplicaciones web progresivas (PWA) y la implementación de tecnologías como React, Angular, Node.js y Laravel en el desarrollo de aplicaciones web',
+  openGraph: {
+    title: 'Desarrollo web en latinoamerica',
+    description: 'En los últimos años, el desarrollo web en Latinoamérica ha experimentado un crecimiento significativo, impulsado por el aumento en el acceso a Internet y la adopción de tecnologías digitales.'
+  }
 }
 
 export default function RootLayout ({ children }) {
