42311ce0612dd194819b7077ffc8caa21167cf39
ANTHONY PEREZ <anthony.perez1@utp.ac.pa>
2024-04-07 22:43:04 -0500

    fit: jsx-closing-tag-location

--- src/app/blog/(Posts)/desarrollo-web-latinoamerica/startups.data.js
+++ src/app/blog/(Posts)/desarrollo-web-latinoamerica/startups.data.js
@@ -13,29 +13,36 @@ export default [
   },
   {
     nombre: 'Crehana',
-    descripcion: <p>
-      Es una plataforma educativa en línea que ofrece una amplia variedad de cursos y programas de formación en áreas como diseño, desarrollo web, marketing digital, fotografía,
-      negocios, entre otros.<span className=' font-bold bg-gradient-to-br from-sky-500 to-cyan-400 bg-clip-text text-transparent'>  Fundada en Perú en 2015 </span>, Crehana se ha convertido en una opción popular para estudiantes y profesionales que buscan mejorar sus habilidades y conocimientos en diferentes campos a través de cursos interactivos y tutoriales en video. La plataforma brinda acceso a contenido de alta calidad creado por expertos
-      y facilita el aprendizaje a tu propio ritmo desde cualquier lugar con conexión a Internet.
-    </p>,
+    descripcion: (
+      <p>
+        Es una plataforma educativa en línea que ofrece una amplia variedad de cursos y programas de formación en áreas como diseño, desarrollo web, marketing digital, fotografía,
+        negocios, entre otros.<span className=' font-bold bg-gradient-to-br from-sky-500 to-cyan-400 bg-clip-text text-transparent'>  Fundada en Perú en 2015 </span>, Crehana se ha convertido en una opción popular para estudiantes y profesionales que buscan mejorar sus habilidades y conocimientos en diferentes campos a través de cursos interactivos y tutoriales en video. La plataforma brinda acceso a contenido de alta calidad creado por expertos
+        y facilita el aprendizaje a tu propio ritmo desde cualquier lugar con conexión a Internet.
+      </p>
+    ),
     srcimage: '/assets/crehana.webp',
     className: 'py-5'
   },
   {
     nombre: 'd.local',
-    descripcion: <p>
-      Es una empresa de tecnología financiera con sede en Uruguay que ofrece servicios de pagos digitales y soluciones de comercio electrónico para empresas en mercados emergentes como América Latina, África y Asia. Su plataforma permite aceptar múltiples métodos de pago locales, gestionar la conversión de monedas,
-      cumplir con regulaciones locales y brindar soporte multilingüe, facilitando transacciones seguras y eficientes en mercados internacionales.
-    </p>,
+    descripcion:
+    (
+      <p>
+        Es una empresa de tecnología financiera con sede en Uruguay que ofrece servicios de pagos digitales y soluciones de comercio electrónico para empresas en mercados emergentes como América Latina, África y Asia. Su plataforma permite aceptar múltiples métodos de pago locales, gestionar la conversión de monedas,
+        cumplir con regulaciones locales y brindar soporte multilingüe, facilitando transacciones seguras y eficientes en mercados internacionales.
+      </p>
+    ),
     srcimage: '/assets/dlocal.png',
     className: 'py-5'
   },
   {
     nombre: 'Ualá',
-    descripcion: <p>
-      Es una empresa fintech argentina que ofrece una tarjeta de débito prepaga y una aplicación móvil para gestionar finanzas de forma gratuita y sin cargos adicionales. La tarjeta permite hacer pagos en comercios físicos y en línea, transferencias y recargas de saldo, mientras que la app ofrece herramientas para control de gastos y seguimiento de movimientos.
-      Ualá ha tenido un rápido crecimiento en Argentina y busca expandirse en América Latina.
-    </p>,
+    descripcion: (
+      <p>
+        Es una empresa fintech argentina que ofrece una tarjeta de débito prepaga y una aplicación móvil para gestionar finanzas de forma gratuita y sin cargos adicionales. La tarjeta permite hacer pagos en comercios físicos y en línea, transferencias y recargas de saldo, mientras que la app ofrece herramientas para control de gastos y seguimiento de movimientos.
+        Ualá ha tenido un rápido crecimiento en Argentina y busca expandirse en América Latina.
+      </p>
+    ),
     srcimage: '/assets/uala.webp',
     className: 'py-5'
   }
