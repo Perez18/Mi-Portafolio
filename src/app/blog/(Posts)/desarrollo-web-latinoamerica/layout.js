db825429c71b28080ffcb1493651278222184f07
ANTHONY PEREZ <anthony.perez1@utp.ac.pa>
2024-04-21 05:13:34 -0500

    feat:change de image de openGraph

--- src/app/blog/(Posts)/desarrollo-web-latinoamerica/layout.js
+++ src/app/blog/(Posts)/desarrollo-web-latinoamerica/layout.js
@@ -6,7 +6,7 @@ export const metadata = {
   openGraph: {
     title: 'Desarrollo web en latinoamerica',
     description: 'En los últimos años, el desarrollo web en Latinoamérica ha experimentado un crecimiento significativo, impulsado por el aumento en el acceso a Internet y la adopción de tecnologías digitales.',
-    images: '/astronauta.png'
+    images: '/blog.avif'
   },
   metadataBase: new URL('https://www.anthonyperez.xyz'),
   alternates: {
